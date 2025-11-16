ff3717043ec7c05a24c9029ce2b22143af5d3b8a
Abu Bakar <abubakar@Abus-MacBook-Air.local>
2025-11-16 22:18:21 +0500

    Toaster Position set

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ export default function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
 
-        <Toaster />
+        <Toaster position="top-right" />
       </div>
     </Router>
   );
