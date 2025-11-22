6b99461d02e79c429b6f31749c645b5724d7179f
Abu Bakar <abubakar@Abus-MacBook-Air.local>
2025-11-23 00:41:58 +0500

    Centralized Home page for each occassion

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { ConvexReactClient } from "convex/react";
 import { ConvexProvider } from "convex/react";
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
 import { queryClient } from "./data";
@@ -22,14 +21,12 @@ const convex = new ConvexReactClient(convexUrl);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <ConvexProvider client={convex}>
-          <ConvexAuthProvider client={convex}>
-            <App />
-          </ConvexAuthProvider>
-        </ConvexProvider>
-      </QueryClientProvider>
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConvexProvider client={convex}>
+        <ConvexAuthProvider client={convex}>
+          <App />
+        </ConvexAuthProvider>
+      </ConvexProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
