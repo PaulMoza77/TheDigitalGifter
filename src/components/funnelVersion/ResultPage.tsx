9ea572ee366cde13e40261669c57177da2935045
Paul Moza <rentalcarsoradea@gmail.com>
2026-03-09 11:26:22 +0200

    error fixed

--- src/components/funnelVersion/ResultPage.tsx
+++ src/components/funnelVersion/ResultPage.tsx
@@ -19,7 +19,7 @@ import {
 
 const publicSupabase = createClient(
   "https://rmdsnpckutsucabledqz.supabase.co",
-  "PUNE_AICI_ANON_KEY_UL_TAU_COMPLET_CARE_INCEPE_CU_eyJ",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtZHNucGNrdXRzdWNhYmxlZHF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MjE2NTksImV4cCI6MjA4NjM5NzY1OX0.yHUiSnsvCyjXkLaazuumVcEL4d0ChdwFaaFR4YXkkCI",
   {
     auth: {
       persistSession: false,
